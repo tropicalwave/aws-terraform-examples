6b79263d677f3de7c69b7754f8a752fbe9fd7907
tropicalwave <44818573+tropicalwave@users.noreply.github.com>
2024-08-06 22:12:04 +0200

    add example for private api gateway

---
+++ alb-private-api-lambda/index.js
@@ -0,0 +1,19 @@
+// index.js
+
+exports.handler = async (event) => {
+    console.log('Event: ', JSON.stringify(event, null, 2));
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: 'Hello from Lambda!',
+            timestamp: new Date().toISOString(),
+            event: event
+        })
+    };
+
+    return response;
+};
